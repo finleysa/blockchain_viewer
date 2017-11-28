01e5215db6121b4934bc5a26cdf7b2e3e48991de
Steven Finley <steve@Stevens-MacBook-Pro.local>
2017-11-27 22:30:10 -0500

    server started

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const users = require('./routes/users');
+const config = require('./config/database');
+
+mongoose.connect(config.database, {useMongoClient: true});
+
+mongoose.connection.on('connected', () => {
+  console.log("Connected to db " + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error("Database error: " + err);
+});
+
+const app = express();
+const port = 3000;
+
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json());
+
+app.listen(port, ()=> {
+  console.info('listening on 3000');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.use('/users', users);
