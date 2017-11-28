1d2ce3505bc4af10eefea988f1ba448a3b1ff599
Steven Finley <steve@Stevens-MacBook-Pro.local>
2017-11-27 23:17:52 -0500

    register complete

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
-router.get('/register', (req, res, next) => {
-  res.send("register");
+const User = require('../models/user');
+
+
+router.post('/register', (req, res, next) => {
+  console.log(req.body);
+
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({success: false, msg: err})
+    } else {
+      res.json({success: true, msg: 'Registered user'})
+    }
+  })
 });
 
 router.post('/authenticate', (req, res, next) => {
@@ -11,10 +31,6 @@ router.post('/authenticate', (req, res, next) => {
 
 router.get('/profile', (req, res, next) => {
   res.send("profile");
-});
-
-router.get('/validate', (req, res, next) => {
-  res.send("validate");
 });
 
 module.exports = router;
